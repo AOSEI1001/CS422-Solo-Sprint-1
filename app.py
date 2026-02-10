a851e68763f00d55ccf930ee76e70099cb899925
AOSEI1001 <abisaamankwah51@yahoo.com>
2026-02-10 15:03:04 -0500

    pylint updated code

--- app.py
+++ app.py
@@ -2,12 +2,8 @@
 
 from flask import Flask
 from views import main_blueprint
-import os
 
-app = Flask(__name__, 
-            template_folder='templates',
-            static_folder='assets',
-            static_url_path='/assets')
+app = Flask(__name__, template_folder='templates', static_folder='assets', static_url_path='/assets')
 
 # Register blueprint for routes
 app.register_blueprint(main_blueprint)
